57d0334dce2e8c35f7a85af1924403b3087f2cc4
LIJA JOSE <105269384+lija12-3@users.noreply.github.com>
2024-08-12 19:08:10 +0530

    Create index.test.js

---
+++ index.test.js
@@ -0,0 +1,19 @@
+{
+  "name": "my-node-app",
+  "version": "1.0.0",
+  "description": "A simple Node.js application",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  },
+  "devDependencies": {
+    "jest": "^29.0.0"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
