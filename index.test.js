a226b2f1c70268868800a0679963897c3bc9a233
LIJA JOSE <105269384+lija12-3@users.noreply.github.com>
2024-08-12 19:14:04 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,19 +1,14 @@
-{
-  "name": "my-node-app",
-  "version": "1.0.0",
-  "description": "A simple Node.js application",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1"
-  },
-  "devDependencies": {
-    "jest": "^29.0.0"
-  },
-  "engines": {
-    "node": ">=14.0.0"
-  }
-}
+const request = require('supertest');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+describe('GET /', () => {
+  it('should return Hello World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Hello World!');
+  });
+});
